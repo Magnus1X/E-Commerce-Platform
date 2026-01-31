c16f31d1a9e6d4785bc32e5f7242a55c6d4da040
Saurav Kumar <95sauravkumar95@gmail.com>
2026-02-01 02:43:16 +0530

    Making Perfect Backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import categoryRoutes from './routes/category.routes';
+import productRoutes from './routes/product.routes';
 import { ErrorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
