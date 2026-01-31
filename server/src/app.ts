5d5a27bfe5b342eda508fd2b8a6fbd5fec9e9c4f
Saurav Kumar <magnus@Sauravs-MacBook-Pro.local>
2026-02-01 02:39:57 +0530

    Update app.ts: Mount Category routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
+import categoryRoutes from './routes/category.routes';
 import { ErrorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
+app.use('/api/categories', categoryRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
