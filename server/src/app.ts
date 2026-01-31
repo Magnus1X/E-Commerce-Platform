5313baf3ff78a4cb4e2b8669070a67ab900a8a98
Saurav Kumar <magnus@Sauravs-MacBook-Pro.local>
2026-02-01 02:45:18 +0530

    Update app.ts: Mount Order routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import categoryRoutes from './routes/category.routes';
 import productRoutes from './routes/product.routes';
+import orderRoutes from './routes/order.routes';
 import { ErrorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
